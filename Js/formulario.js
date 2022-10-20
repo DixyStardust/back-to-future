2eb96ef5dcfb0125230eb8809b4446a0a84ebb70
M Victoria <mvittoria7@gmail.com>
2022-10-20 13:31:34 -0300

    Cambios realizados en formulario

--- Js/formulario.js
+++ Js/formulario.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContenLoaded", function() {
+  document.getElementById("formulario").addEventListener('submit', validarFormulario);
+});
+
+function validarFormulario(e){
+  e.preventDefault();
+  let nombreUsuario = document.getElementById('nombre').value;
+  if(nombreDeUsuario.length === 0){
+    alert("Por favor, escribe tu nombre tu nombre.");
+    return;
+  }
+  this.submit();
+}
+
 /*const btnEnviar = document.getElementById('btn-enviar');
 
 const validar = (e) => {
